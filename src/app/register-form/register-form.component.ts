52e7fe73b343944117ba732cc6fda43b935514dc
Mohammed-ALAmri <36080250+Mohammed-ALAmri@users.noreply.github.com>
2019-06-23 14:01:08 -0700

    Update register-form.component.ts

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-form.component.css']
 })
 export class RegisterFormComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
   }
 
+  registerUser(event){
+    event.preventDefault();
+    const target = event.target
+    let first = target.querySelector('#first').value
+    let last = target.querySelector('#last').value
+    let phoneNumber = target.querySelector('#phoneNumber').value
+    let Password = target.querySelector('#Password').value
+    let Password2 = target.querySelector('#Password2').value
+  }
 }
