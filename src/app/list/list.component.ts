dc234a59fac3287a68866e74c619c7bcc22511f3
Mohammed-ALAmri <36080250+Mohammed-ALAmri@users.noreply.github.com>
2019-06-26 12:37:14 -0700

    Update list.component.ts

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -9,11 +9,17 @@ import { EmployeeService } from '../employee.service';
 export class ListComponent implements OnInit {
 
   private employees = [];
-
+  pageOfItems: Array<any>;
+  
   constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
     this.employeeService.listEmp().subscribe(data => this.employees = data)
   }
+
+  onChangePage(pageOfItems: Array<any>) {
+    // update current page of items
+    this.pageOfItems = pageOfItems;
+}
 
 }
