d8a52da6901b460ed6f8fa009bc1183294b7b9a2
Mohammed-ALAmri <36080250+Mohammed-ALAmri@users.noreply.github.com>
2019-06-30 09:00:29 -0700

    Update list.component.ts

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../employee.service';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from '../employee' ; 
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'list',
@@ -9,9 +12,12 @@ import { EmployeeService } from '../employee.service';
 export class ListComponent implements OnInit {
 
   private employees = [];
+  private emp = [];
   pageOfItems: Array<any>;
-  
-  constructor(private employeeService: EmployeeService) { }
+  private newEmployee: object = {};
+  url = "http://localhost:5555/employees"
+
+  constructor(private http: HttpClient, private employeeService: EmployeeService) { }
 
   ngOnInit() {
     this.employeeService.listEmp().subscribe(data => this.employees = data)
@@ -20,6 +26,30 @@ export class ListComponent implements OnInit {
   onChangePage(pageOfItems: Array<any>) {
     // update current page of items
     this.pageOfItems = pageOfItems;
-}
+  }
 
+  addNewEmp(event){
+    const employee = event.target;
+    this.newEmployee = {
+      "id": employee.querySelector('#id').value,
+      "name": employee.querySelector('#name').value,
+      "salary":employee.querySelector('#salary').value
+    };
+    this.employeeService.addEmp(this.newEmployee);
+  }
+
+  deleteEmp(id){
+    this.http.delete(this.url + `/${id}`).toPromise().then(() => {
+      this.http.get<Employee[]>(this.url).subscribe(data => this.employees = data);
+   })
+  }
+
+  searchEmp(event){
+    event.preventDefault();
+    const employee = event.target;
+    this.employeeService.searchEmp(employee.querySelector('#id').value).subscribe(data => {
+      this.emp[0] = data 
+      this.employees = this.emp
+    });
+  }
 }
