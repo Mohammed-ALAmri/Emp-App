66bb28693a677d4b1db989b69f527bef7b7b2f5a
Mohammed-ALAmri <36080250+Mohammed-ALAmri@users.noreply.github.com>
2019-07-01 10:39:02 -0700

    Update add-employee.component.ts

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-employee',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-employee.component.css']
 })
 export class AddEmployeeComponent implements OnInit {
+
+  addEmployeeForm = new FormGroup({
+    id: new FormControl(''),
+    name: new FormControl(''),
+    salary: new FormControl(''),
+    dev: new FormControl(''),
+    birthdate: new FormControl(''),
+    idp: new FormControl(''),
+    namep: new FormControl(''),
+    startp: new FormControl('')
+  });
 
   constructor() { }
 
