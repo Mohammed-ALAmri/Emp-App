cd0d9ff893792d928be1b763fab9423cf8940689
Mohammed-ALAmri <36080250+Mohammed-ALAmri@users.noreply.github.com>
2019-07-01 15:29:21 -0700

    Update employee.ts

--- src/app/employee.ts
+++ src/app/employee.ts
@@ -1,5 +1,14 @@
 export interface Employee{
     id: number,
     name: string,
-    salary: number
+    salary: number,
+    birthdate: Date,
+    isDev: boolean,
+    projects: [
+      {
+        id: number,
+        name: string,
+        startDate: Date
+      }
+    ]
 }
