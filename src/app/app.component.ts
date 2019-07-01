b487b81d4431c374e1ed56b9353f918d5a037bc9
Mohammed-ALAmri <36080250+Mohammed-ALAmri@users.noreply.github.com>
2019-07-01 10:38:59 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = 'Management App';
 
-  ngOnInit(){
-    
-  }
-}
+export class AppComponent {  }
