3ab3adb8c0cf335b53213711c9627e3768515ae9
Mohammed-ALAmri <36080250+Mohammed-ALAmri@users.noreply.github.com>
2019-07-03 14:31:29 -0700

    Update list.component.ts

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -27,28 +27,9 @@ export class ListComponent implements OnInit {
     this.pageOfItems = pageOfItems;
   }
 
-  addNewEmp(event){
-    const employee = event.target;
-    this.newEmployee = {
-      "id": employee.querySelector('#id').value,
-      "name": employee.querySelector('#name').value,
-      "salary":employee.querySelector('#salary').value
-    };
-    this.employeeService.addEmp(this.newEmployee);
-  }
-
   deleteEmp(id){
     this.http.delete(this.url + `/${id}`).toPromise().then(() => {
       this.http.get<Employee[]>(this.url).subscribe(data => this.employees = data);
    })
-  }
-
-  searchEmp(event){
-    event.preventDefault();
-    const employee = event.target;
-    this.employeeService.searchEmp(employee.querySelector('#id').value).subscribe(data => {
-      this.emp[0] = data 
-      this.employees = this.emp
-    });
   }
 }
