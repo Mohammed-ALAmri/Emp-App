14d1e7c9ca65e25820d47390c7d184f0361cb773
Mohammed-ALAmri <36080250+Mohammed-ALAmri@users.noreply.github.com>
2019-07-07 14:50:46 -0700

    Create employee.service.ts

---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from '../interfaces/employee' ;
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  url = "http://localhost:5555/employees"
+
+  constructor(private http: HttpClient) { }
+
+  listEmp(): Observable <Employee[]>{
+    return this.http.get<Employee[]>(this.url);
+  }
+
+  addEmp(newEmployee: object){
+    this.http.post(this.url, newEmployee).subscribe();
+  }
+
+  searchEmp(id): Observable <Employee[]>{
+    return this.http.get<Employee[]>(this.url+`/${id}`);
+  }
+}
