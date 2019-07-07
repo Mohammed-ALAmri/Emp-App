fbd6d72a39782bcf3ec48d3146e5d93641afa2b6
Mohammed-ALAmri <36080250+Mohammed-ALAmri@users.noreply.github.com>
2019-07-07 14:51:18 -0700

    Update register-form.component.ts

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AdminService } from '../services/admin.service';
+
+
 
 @Component({
   selector: 'register-form',
@@ -6,19 +10,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-form.component.css']
 })
 export class RegisterFormComponent implements OnInit {
-  
-  constructor() { }
+
+  private admin =[]
+
+  constructor(private fb: FormBuilder, private adminService: AdminService) { }
+
+  addAdminForm = this.fb.group({
+    id: ['', Validators.required],
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+    password: ['', Validators.required],
+    rePassword: ['', Validators.required]
+  });
 
   ngOnInit() {
+    this.adminService.getAdmin().subscribe(data => this.admin = data)
   }
 
-  registerUser(event){
-    event.preventDefault();
-    const target = event.target
-    let first = target.querySelector('#first').value
-    let last = target.querySelector('#last').value
-    let phoneNumber = target.querySelector('#phoneNumber').value
-    let Password = target.querySelector('#Password').value
-    let Password2 = target.querySelector('#Password2').value
+  registerUser(){
+    let data = this.addAdminForm.value;
+    let newAdmin = {}
+    let flag = false;
+    this.admin.forEach(element => {
+      if(element.phone === data.phone){
+        flag = true;
+      }
+    });
+    if(flag){
+      console.log('Same Name')
+    }else{
+      if(data.password === data.rePassword){
+        newAdmin = {
+          id: data.id,
+          name: data.name,
+          phone : data.phone,
+          password: data.password
+        }
+        this.adminService.addAdmin(newAdmin);
+      }
+      else{
+        console.log("Not equal pass")
+      }
+    }
+
   }
 }
