d6cb1e94fe740432dce46d96418a53f873e9df27
Mohammed-ALAmri <36080250+Mohammed-ALAmri@users.noreply.github.com>
2019-07-07 14:51:30 -0700

    Update login-form.component.ts

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AdminService } from '../services/admin.service';
 
 @Component({
   selector: 'login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
+
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  private admin =[]
+
+  constructor(private fb: FormBuilder, private adminService: AdminService) { }
+
+  logInAdminForm = this.fb.group({
+    phone: ['', Validators.required],
+    password: ['', Validators.required],
+  });
 
   ngOnInit() {
+    this.adminService.getAdmin().subscribe(data => this.admin = data)
+  }
+
+  logInUser(){
+    let data = this.logInAdminForm.value;
+    this.adminService.logInUser(data)
   }
 
 }
