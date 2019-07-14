629bcd387e17290b38a830b0cf3e2f97d9108cc9
Mohammed-ALAmri <36080250+Mohammed-ALAmri@users.noreply.github.com>
2019-07-14 14:48:23 -0700

    Update login-form.component.ts

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AdminService } from '../services/admin.service';
+import { Router } from "@angular/router"
 
 @Component({
   selector: 'login-form',
@@ -10,22 +11,22 @@ import { AdminService } from '../services/admin.service';
 
 export class LoginFormComponent implements OnInit {
 
-  private admin =[]
+  private admin =[];
 
-  constructor(private fb: FormBuilder, private adminService: AdminService) { }
+  constructor(private fb: FormBuilder, private adminService: AdminService, private router: Router) { }
 
   logInAdminForm = this.fb.group({
     phone: ['', Validators.required],
     password: ['', Validators.required],
   });
 
-  ngOnInit() {
-    this.adminService.getAdmin().subscribe(data => this.admin = data)
-  }
+  ngOnInit() { }
 
   logInUser(){
     let data = this.logInAdminForm.value;
-    this.adminService.logInUser(data)
+    this.adminService.loginAdmin(data).subscribe(
+      res => console.log(res),
+      err => console.log(err))
+    // this.router.navigate(['/empltoyeelist']);
   }
-
 }
