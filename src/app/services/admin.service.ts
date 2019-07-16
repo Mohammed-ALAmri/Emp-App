c5939c4316faca44d65cf844d63e235dafe57abb
Mohammed-ALAmri <36080250+Mohammed-ALAmri@users.noreply.github.com>
2019-07-16 14:36:47 -0700

    Update admin.service.ts

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Admin } from '../interfaces/admin' ;
-import {Observable} from 'rxjs';
+import { Router } from '@angular/router'
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +10,7 @@ export class AdminService {
   private register_url ="http://localhost:3000/api/register" 
   private login_url ="http://localhost:3000/api/login" 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   registerAdmin(newAdmin: object){
     return this.http.post<any>(this.register_url, newAdmin);
@@ -19,5 +18,17 @@ export class AdminService {
 
   loginAdmin(admin){
     return this.http.post<any>(this.login_url, admin);
+  }
+
+  loggedIn() {
+    return !!localStorage.getItem('token')    
+  }
+
+  logout() {
+    localStorage.removeItem('token')
+  }
+
+  getToken() {
+    return localStorage.getItem('token')
   }
 }
