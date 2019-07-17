318144d34e5d023ecb485dc3d4ff7891fa3472a1
Mohammed-ALAmri <36080250+Mohammed-ALAmri@users.noreply.github.com>
2019-07-17 09:40:42 -0700

    Update token-interceptor.service.ts

--- src/app/services/token-interceptor.service.ts
+++ src/app/services/token-interceptor.service.ts
@@ -4,6 +4,7 @@ import { HttpInterceptor } from '@angular/common/http'
 import { AdminService } from './admin.service';
 
 @Injectable()
+
 export class TokenInterceptorService implements HttpInterceptor {
 
   constructor(private injector: Injector){}
@@ -13,22 +14,7 @@ export class TokenInterceptorService implements HttpInterceptor {
       setHeaders: {
         Authorization: `Bearer ${adminService.getToken()}`
       }
-      // headers: req.headers.set('Authorization', 'bearer ' + adminService.getToken())
     })
     return next.handle(tokenizedReq)
   }
-
 }
-
-// constructor(private injector: Injector){}
-// intercept(req, next) {
-//   let adminService = this.injector.get(AdminService)
-//   let tokenizedReq = req.clone(
-//     {
-//       headers: req.headers.set('Authorization', 'bearer ' + adminService.getToken())
-//     }
-//   )
-//   return next.handle(tokenizedReq)
-// }
-
-// }
